ae2355b6459802fd2ef294087861320bddc769d0
qvisten999 <jqjqvist@gmail.com>
2021-07-20 14:24:03 +0200

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,53 @@
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CookieConsent from "@toitware/cookie-consent";
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import theme from "./assets/theme/theme";
+import AppBar from "./components/AppBar";
+import Packages from "./data/packages.json";
 
-function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = Packages;
+
+class App extends React.Component {
+  loadCrispChat() {
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "bf6d2763-2d2d-4ebb-b8eb-da83a95ec3bf";
+
+    (function () {
+      const d = document;
+      const s = d.createElement("script");
+
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = true;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+  }
+
+  async componentDidMount() {
+    this.loadCrispChat();
+    //const response = await fetch("https://pkg.infra.toit.io/api/v1/packages");
+    console.log("response: ", data);
+  }
+
+  componentWillUnmount() {}
+
+  render() {
+    let segmentAPIKey = "";
+    if (typeof document !== `undefined`) {
+      // Check if the meta segment-key is set.
+      const segmentKeyDOM = document.querySelector('meta[name="segment-key"]');
+      if (segmentKeyDOM) {
+        segmentAPIKey = segmentKeyDOM.getAttribute("content") || segmentAPIKey;
+      }
+    }
+    return (
+      <MuiThemeProvider theme={theme}>
+        <AppBar />
+        {/* TODO implement tracking, ask Jesper if we should do everything server side */}
+        <CookieConsent segmentKey={segmentAPIKey} show={false} changeConsent={false} />
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,140 @@
+import {
+  alpha,
+  AppBar as Bar,
+  Button,
+  createStyles,
+  Divider,
+  InputBase,
+  Theme,
+  Toolbar,
+  Typography,
+  WithStyles,
+  withStyles,
+} from "@material-ui/core";
+import React from "react";
+import PackagesTestFile from "../data/packages.json";
+import { SearchIcon, ToitLogo } from "../misc/icons";
+
+const index = 5;
+const store = PackagesTestFile;
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    toolbarTop: {
+      minHeight: 42,
+      backgroundColor: theme.palette.primary.dark,
+    },
+    toolbarBottom: {
+      height: 84,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    search: {
+      position: "relative",
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: theme.palette.primary.light,
+      "&:hover": {
+        backgroundColor: alpha(theme.palette.primary.light, 0.25),
+      },
+      marginLeft: theme.spacing(2),
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(4),
+        width: "100%",
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    inputRoot: {
+      color: "inherit",
+      borderWidth: 1,
+      borderColor: theme.palette.primary.dark,
+      borderStyle: "solid",
+      height: "42px",
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+    },
+    searchButton: {
+      height: 42,
+      borderRadius: 0,
+      width: 80,
+      [theme.breakpoints.up("sm")]: {
+        width: 160,
+      },
+    },
+  });
+
+type AppBarProps = WithStyles<typeof styles>;
+
+class AppBar extends React.PureComponent<AppBarProps> {
+  render() {
+    return (
+      <Bar color="secondary">
+        <Toolbar className={this.props.classes.toolbarTop}>
+          <Typography variant="h6" className={this.props.classes.title}>
+            {" "}
+          </Typography>
+          <Button color="secondary" href="https://toit.io" target="blank">
+            Website
+          </Button>
+          <Button color="secondary" href="https://console.toit.io" target="blank">
+            Console
+          </Button>
+          <Button color="secondary" href="https://docs.toit.io" target="blank">
+            Documentation
+          </Button>
+          <Button color="secondary" href="https://libs.toit.io" target="blank">
+            Library
+          </Button>
+          <Button color="secondary" href="https://slack.toit.io" target="blank">
+            Community
+          </Button>
+        </Toolbar>
+        <Divider />
+        <Toolbar className={this.props.classes.toolbarBottom}>
+          <Typography className={this.props.classes.title}>
+            <ToitLogo />
+          </Typography>
+          <div className={this.props.classes.search}>
+            <div className={this.props.classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              fullWidth
+              placeholder="Search…"
+              classes={{
+                root: this.props.classes.inputRoot,
+                input: this.props.classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <Button className={this.props.classes.searchButton} color="primary" variant="contained">
+            Search
+          </Button>
+        </Toolbar>
+      </Bar>
+    );
+  }
+}
+
+export default withStyles(styles)(AppBar);

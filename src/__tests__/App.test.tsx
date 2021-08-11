e8ee604421502ffe11aabf6c0aa9bf6b483a260a
qvisten999 <jqjqvist@gmail.com>
2021-08-11 07:56:02 +0200

    remove tests

--- src/__tests__/App.test.tsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import App from "../App";
-
-test("true expects true", () => {
-  expect(true).toEqual(true);
-});
